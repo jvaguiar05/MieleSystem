37f635d3a734c8e2408fa07ddab5652f7d573364
Joao Vitor <jvads2005@gmail.com>
2025-08-05 16:31:31 -0300

    feat: add ISoftDeletable interface for logical deletion of entities

---
+++ src/MieleSystem.Domain/Common/Utils/ISoftDeletable.cs
@@ -0,0 +1,14 @@
+namespace MieleSystem.Domain.Common.Utils;
+
+/// <summary>
+/// Marca uma entidade como deletável logicamente.
+/// Permite que a entidade seja "deletada" sem ser removida fisicamente do banco de dados.
+/// </summary>
+public interface ISoftDeletable
+{
+    bool IsDeleted { get; }
+    DateTime? DeletedAt { get; }
+
+    void Delete();
+    void Restore();
+}
